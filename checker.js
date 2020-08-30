f985fae0cdc0efccc9c5cb30bf69e00ce168c4e5
Prajyot Ture <47532830+prajyotture@users.noreply.github.com>
2020-08-30 20:21:37 +0530

    Add files via upload

---
+++ checker.js
@@ -0,0 +1,16 @@
+const expect = require('chai').expect;
+const b=require("./bpmCheck");
+const r=require("./respCheck");
+const s=require("./spo2Check");
+function vitalsAreOk(bpm, spo2, respRate) {
+    if((!IsBPMUnderControl(bpm)) || (!IsRespirationOk(respRate)) ||(!IsSPO2UnderControl(spo2))){
+        return true;
+    }
+    console.log("Everything is under control");
+    return false;
+}
+
+expect(vitalsAreOk(100, 95, 70)).to.be.true;
+expect(vitalsAreOk(50, 95, 70)).to.be.false;
+
+console.log('checker is done');
