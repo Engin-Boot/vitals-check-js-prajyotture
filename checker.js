4a3523f8d4216e75e3c764fed90d4a56996ef2f0
Prajyot Ture <47532830+prajyotture@users.noreply.github.com>
2020-08-30 21:03:25 +0530

    Update checker.js

--- checker.js
+++ checker.js
@@ -1,9 +1,10 @@
-const expect = require('chai').expect;
-const b=require("./bpmCheck.js");
-const r=require("./respCheck.js");
-const s=require("./spo2Check.js");
+//const expect = require('chai').expect;
+const bpmChecker=require("./bpmCheck.js");
+const respChecker=require("./respCheck.js");
+const spo2Checker=require("./spo2Check.js");
+
 function vitalsAreOk(bpm, spo2, respRate) {
-    if(( (IsBPMUnderControl(bpm)==false) || (IsSPO2UnderControl(spo2)==false) || (IsRespirationOk(respRate)==false) )==false) {
+    if(  bpmChecker(bpm) || respChecker(respRate) || spo2Checker(spo2) ) {
         return false;
     }
     console.log("Everything is under control");
@@ -12,5 +13,5 @@ function vitalsAreOk(bpm, spo2, respRate) {
 
 expect(vitalsAreOk(100, 95, 70)).to.be.true
 expect(vitalsAreOk(50, 95, 70)).to.be.false;
-
+//vitalsAreOk(100,95,70);
 console.log('checker is done');
